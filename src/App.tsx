46d324dd2ed040be3474237c8213eb23b52a7564
D.Sriver <duez.s@proton.me>
2024-09-04 14:02:31 +0200

    ✨ Implement protected routes for authenticated users.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
 import SignIn from './components/SignIn';
 import TransactionList from './components/TransactionList';
 import User from './components/User';
@@ -16,8 +17,22 @@ export default function App() {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/sign-in" element={<SignIn />} />
-						<Route path="/user" element={<User />} />
-						<Route path="/transactions" element={<TransactionList />} />
+						<Route
+							path="/profile"
+							element={
+								<ProtectedRoute>
+									<User />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/transactions"
+							element={
+								<ProtectedRoute>
+									<TransactionList />
+								</ProtectedRoute>
+							}
+						/>
 						<Route path="*" element={<NotFound />} />
 					</Routes>
 				</main>
