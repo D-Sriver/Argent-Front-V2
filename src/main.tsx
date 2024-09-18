9b3e333f2bfdc4f68447de323a4d0a1f20af9e8c
D.Sriver <duez.s@proton.me>
2024-09-18 13:21:16 +0200

    💄 Revert CSS to original design

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
-import './index1.css';
+import './index.css';
 import { store } from './store/store';
 
 createRoot(document.getElementById('root')!).render(
